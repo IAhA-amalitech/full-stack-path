0770948710eae10ec0831313bb18a5ef3561c601
IAhA-amalitech <97875943+IAhA-amalitech@users.noreply.github.com>
2022-01-25 10:56:04 +0000

    Secet Message project 2 v1

---
+++ secret_message.js
@@ -0,0 +1,50 @@
+let secretMessage = ['Learning', 'is', 'not', 'about', 'what', 'you', 'get', 'easily', 'the', 'first', 'time,', 'it', 'is', 'about', 'what', 'you', 'can', 'figure', 'out.', '-2015,', 'Chris', 'Pine,', 'Learn', 'JavaScript'];
+
+// step 1
+secretMessage.pop();
+
+// step 2
+console.log(secretMessage.length);
+
+// step 3
+secretMessage.push('to','Program');
+
+// step 4
+secretMessage[secretMessage.indexOf['easily']] = 'right';
+
+// step 5
+secretMessage.shift();
+
+// step 6
+secretMessage.unshift('Programming');
+
+// step 7
+secretMessage.splice(6,5,'know');
+
+// step 8
+console.log(secretMessage.join(' '));
+let secretMessage = ['Learning', 'is', 'not', 'about', 'what', 'you', 'get', 'easily', 'the', 'first', 'time,', 'it', 'is', 'about', 'what', 'you', 'can', 'figure', 'out.', '-2015,', 'Chris', 'Pine,', 'Learn', 'JavaScript'];
+
+// step 1
+secretMessage.pop();
+
+// step 2
+console.log(secretMessage.length);
+
+// step 3
+secretMessage.push('to','Program');
+
+// step 4
+secretMessage[secretMessage.indexOf['easily']] = 'right';
+
+// step 5
+secretMessage.shift();
+
+// step 6
+secretMessage.unshift('Programming');
+
+// step 7
+secretMessage.splice(6,5,'know');
+
+// step 8
+console.log(secretMessage.join(' '));
